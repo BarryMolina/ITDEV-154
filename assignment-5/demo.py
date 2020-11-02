6e83efb4d05a0c80a499c9b1fc7aaaab8f4d68b3
Barry Molina <bazzaboy@gmail.com>
2020-11-01 20:25:51 -0600

    Tower of Hanoi

--- assignment-5/demo.py
+++ assignment-5/demo.py
@@ -1,5 +1,10 @@
+import time
+from collections import deque
+
 
 def find_factorial():
+    print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+    print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
     while True:
         print("\n\nEnter a number to find the factorial of or "
                 "\npress <enter> to return to the Main Menu")
@@ -13,12 +18,16 @@ def find_factorial():
 
         print(f"\n{num}!: {fact}")
 
+
 def factorial(num):
     if num == 0:
         return 1
     return num * factorial(num - 1)
 
+
 def convert_base():
+    print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+    print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
     while True:
         print("\n\nEnter a number to convert or "
                 "\npress <enter> to return to the Main Menu")
@@ -58,29 +67,130 @@ def convert(num, base, converted):
         converted.append(chr(rem - 10 + ord('A')))
 
 
-def gcd():
-    pass
+def find_gcd():
+    print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+    print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+    while True:
+        print("\n\nEnter first number or "
+                "\npress <enter> to return to the Main Menu")
+
+        ans = input("\n>>> ")
+        if not ans:
+            break
+        num1 = int(ans)
+
+        print("\n\nEnter second number or "
+                "\npress <enter> to return to the Main Menu")
+
+        ans = input("\n>>> ")
+        if not ans:
+            break
+        num2 = int(ans)
+
+        print(f"\n\nGCD of {num1} and {num2} is {gcd(num1, num2)}")
+
+
+def gcd(a, b):
+    if b == 0:
+        return a
+    return gcd(b, a % b)
+
 
 def fibonacci():
-    pass
+    print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+    print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+    while True:
+        print("\n\nEnter number of fibonacci numbers to print or"
+                "\npress <enter> to return to the Main Menu")
 
-def hanoi():
-    pass
+        ans = input("\n>>> ")
+        if not ans:
+            break
+        n = int(ans)
+
+        # join a list of strings created from a list of fibonacci terms
+        fibs = ' '.join([str(num) for num in [fib(x) for x in range(0, n)]])
+
+        print(f"\n\n{fibs}")
+
+
+def fib(n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    return fib(n - 1) + fib(n - 2)
+
+
+def tower_of_hanoi():
+    print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+    print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+    while True:
+        print("\n\nEnter number of disks or"
+                "\npress <enter> to return to the Main Menu")
+
+        ans = input("\n>>> ")
+        if not ans:
+            break
+        n = int(ans)
+
+        tower = Tower_Of_Hanoi(n)
+        tower.execute()
+
+
+class Tower_Of_Hanoi():
+    def __init__(self, n):
+        self.n = n
+        self.source = deque(num for num in reversed(range(1, n + 1)))
+        self.temp = deque()
+        self.dest = deque()
+
+    def display_pillars(self):
+        pillars = ""
+        for x in reversed(range(self.n)):
+            pillars += (
+                f"\n{self.source[x] if x < len(self.source) else ''}"
+                f"\t{self.temp[x] if x < len(self.temp) else ''}"
+                f"\t{self.dest[x] if x < len(self.dest) else ''}"
+            )
+
+        print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+        print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+        print("A\tB\tC")
+        print(pillars)
+        print("-------------------")
+        time.sleep(1)
+
+    def execute(self):
+        self.display_pillars()
+        self.hanoi(self.n, self.source, self.temp, self.dest)
+
+    def hanoi(self, n, source, temp, dest):
+        if n == 1:
+            disk = source.pop()
+            dest.append(disk)
+            self.display_pillars()
+            return
+        self.hanoi(n-1, source, dest, temp)
+        dest.append(source.pop())
+        self.display_pillars()
+        self.hanoi(n-1, temp, source, dest)
 
 
 options = [
     find_factorial,
     convert_base,
-    gcd,
+    find_gcd,
     fibonacci,
-    hanoi,
+    tower_of_hanoi,
 ]
 
 def main():
     quit = False
     while not quit:
         print(
-            "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
+            "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
+            "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
             "Welcome to the Recursion app.\n"
             "\nPlease select one of the following options:\n"
             "1: Factorial\n"
